26a87076909bbd1396f1ec895a5da4f003b023f3
jaspin10 <learn@frenchwithjas.ca>
2026-08-03 23:03:43 -0700

    das

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,9 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const client = createClient(
-  import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+const supabaseUrl = 'https://jtzazvkshizmuhezuxwl.supabase.co'
+const supabaseAnonKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp0emF6dmtzaGl6bXVoZXp1eHdsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ2MTE3NDQsImV4cCI6MjEwMDE4Nzc0NH0._j5oYUljKdZ08njGCnbMpbTrYJRAU6H_VdIaMlk7XWQ'
+
+const client = createClient(supabaseUrl, supabaseAnonKey)
 
 let readOnlyMode = false
 
@@ -80,3 +81,4 @@ export const supabase = new Proxy(client, {
     return value
   }
 })
+
